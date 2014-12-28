379dfab7c2fe6ecae7b98de16bb49c683d9d967f
Ian Darwin <ian@darwinsys.com>
2014-12-28 15:22:42 -0500

    Junitize

--- src/test/java/ued/PWTest.java
+++ src/test/java/ued/PWTest.java
@@ -1,23 +1,23 @@
 package ued.test;
 
+import org.junit.*;
+import static org.junit.Assert.*;
+
 import ued.unix.*;
 
 /** Simple program to test the PW class */
 public class PWTest {
-	public static void main(String av[]) {
+
+	@Test
+	public void testReader() {
 		PWReader pr = new PWFileReader("./passwd.txt");
 
 		PW root = pr.getpwnam("root");
-		if (root == null)
-			System.out.println("Root missing");
-		else
-			System.out.println("Root = " + root);
+		assertNotNull("Entry for 'root' missing?", root);
+		System.out.println("Entry for 'root' = " + root);
 
-		PW me = pr.getpwuid(100);
-		if (me == null)
-			System.out.println("Self missing");
-		else
-			System.out.println("Self = " + me);
-
+		PW mrniceguy = pr.getpwuid(100);
+		assertNotNull("Entry for user 100 missing?", mrniceguy);
+		System.out.println("Self = " + mrniceguy);
 	}
-} 
+}
